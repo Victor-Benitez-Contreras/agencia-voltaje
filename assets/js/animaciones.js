d99fcd4bfe29dd409515b0ca90251ce6a4c149e7
Victor Benitez <neoxkrl@gmail.com>
2020-11-28 15:13:13 -0500

    animaciones

---
+++ assets/js/animaciones.js
@@ -0,0 +1,150 @@
+
+gsap.from('.titulo-lo-que-hacemos', {
+    scrollTrigger: '.titulo-lo-que-hacemos',
+    delay: 0.2, 
+    opacity:0, 
+    duration: 1.5, 
+    x: 35, ease: 'Power4.easeOut'
+  });
+
+  gsap.from('.horizonte-procesos-01', {
+    scrollTrigger: '.lo-que-hacemos-servicios',
+    width: 0,
+    delay: 0.3, 
+    opacity:0, 
+    duration: 2.5, 
+    x: -35, ease: 'Power4.easeOut'
+  });
+
+
+/*   gsap.from('.titulo-lo-que-hacemos', {
+    scrollTrigger: '.titulo-lo-que-hacemos',
+    delay: 0.2, 
+    opacity:0, 
+    duration: 1.5, 
+    y: 20, ease: 'Power4.easeOut'
+  }); */
+
+
+  // animaciones lo que ehacemos//
+
+  gsap.from('.diseño-grafico', {
+    scrollTrigger: '.diseño-grafico',
+    delay: 0.4, 
+    opacity:0, 
+    duration: 1.5, 
+    y: 20, ease: 'Power4.easeOut'
+  });
+  
+  gsap.from('.imagen-corporativa', {
+    scrollTrigger: '.imagen-corporativa',
+    delay: 0.6, 
+    opacity:0, 
+    duration: 1.5, 
+    y: 20, ease: 'Power4.easeOut'
+  });
+
+  gsap.from('.diseño-web', {
+    scrollTrigger: '.diseño-web',
+    delay: 0.8, 
+    opacity:0, 
+    duration: 1.5, 
+    y: 20, ease: 'Power4.easeOut'
+  });
+
+  gsap.from('.fotografia-producto', {
+    scrollTrigger: '.fotografia-producto',
+    delay: 0.5, 
+    opacity:0, 
+    duration: 1.5, 
+    y: 20, ease: 'Power4.easeOut'
+  });
+
+  gsap.from('.ilustracion', {
+    scrollTrigger: '.ilustracion',
+    delay: 0.7, 
+    opacity:0, 
+    duration: 1.5, 
+    y: 20, ease: 'Power4.easeOut'
+  });
+
+  gsap.from('.social-manager', {
+    scrollTrigger: '.social-manager',
+    delay: 0.9, 
+    opacity:0, 
+    duration: 1.5, 
+    y: 20, ease: 'Power4.easeOut'
+  });
+
+
+  //portafolio
+
+  gsap.from('.portafolio-titulo', {
+    scrollTrigger: '.lista-portafolio',
+    delay: 0.2, 
+    opacity:0, 
+    duration: 1.5, 
+    x: -35, ease: 'Power4.easeOut'
+  });
+  
+  gsap.from('.horizonte-procesos-02', {
+    scrollTrigger: '.lista-portafolio',
+    width: 0,
+    delay: 0.3, 
+    opacity:0, 
+    duration: 2.5, 
+    x: -35, ease: 'Power4.easeOut'
+  });
+
+
+  gsap.from('.lista-portafolio-item:first-child', {
+    scrollTrigger: '.contenedor-portafolio-img',
+    delay: 0.2, 
+    opacity:0, 
+    duration: 2, 
+    y: 18, ease: 'Power4.easeOut'
+  });
+
+  gsap.from('.lista-portafolio-item:nth-child(2)', {
+    scrollTrigger: '.contenedor-portafolio-img',
+    delay: 0.4, 
+    opacity:0, 
+    duration: 2, 
+    y: 18, ease: 'Power4.easeOut'
+  });
+  
+    gsap.from('.lista-portafolio-item:nth-child(3)', {
+    scrollTrigger: '.contenedor-portafolio-img',
+    delay: 0.6, 
+    opacity:0, 
+    duration: 2, 
+    y: 18, ease: 'Power4.easeOut'
+  });
+
+  gsap.from('.lista-portafolio-item:nth-child(4)', {
+    scrollTrigger: '.contenedor-portafolio-img',
+    delay: 0.8, 
+    opacity:0, 
+    duration: 2, 
+    y: 18, ease: 'Power4.easeOut'
+  });
+
+  gsap.from('.lista-portafolio-item:nth-child(5)', {
+    scrollTrigger: '.contenedor-portafolio-img',
+    delay: 1, 
+    opacity:0, 
+    duration: 2, 
+    y: 18, ease: 'Power4.easeOut'
+  });
+
+  gsap.from('.lista-portafolio-item:nth-child(6)', {
+    scrollTrigger: '.contenedor-portafolio-img',
+    delay: 1.2, 
+    opacity:0, 
+    duration: 2, 
+    y: 18, ease: 'Power4.easeOut'
+  });
+
+
+  //nuestro proceso
+
